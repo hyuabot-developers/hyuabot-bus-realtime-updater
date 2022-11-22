dad6df77825d64c30eed9278f8438dd89c799500
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-22 14:51:59 +0900

    feat: ORM에 FK 선언

--- src/main.py
+++ src/main.py
@@ -10,16 +10,16 @@ from utils.database import get_db_engine
 
 
 async def main():
-    connection = await get_db_engine()
+    connection = get_db_engine()
     session_constructor = sessionmaker(bind=connection)
     session = session_constructor()
     if session is None:
         raise RuntimeError("Failed to get db session")
 
     stop_group = defaultdict(list)
-    stop_query = select([BusRouteStop.stop_id, BusRouteStop.route_id])
-    connection.execute(stop_query)
-    for stop_id, route_id in connection.execute(stop_query):
+    stop_query = select(BusRouteStop.stop_id, BusRouteStop.route_id)
+    session.execute(stop_query)
+    for stop_id, route_id in session.execute(stop_query):
         stop_group[stop_id].append(route_id)
     job_list = []
     for stop_id, route_id_list in stop_group.items():
--- src/models/bus.py
+++ src/models/bus.py
@@ -1,24 +1,55 @@
-from sqlalchemy import PrimaryKeyConstraint, Column
-from sqlalchemy.sql import sqltypes
+import datetime
+
+from sqlalchemy import PrimaryKeyConstraint, ForeignKey, String, Time
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models import BaseModel
+
+
+class BusRoute(BaseModel):
+    __tablename__ = "bus_route"
+    route_id: Mapped[int] = mapped_column(primary_key=True)
+    company_id: Mapped[int] = mapped_column(nullable=False)
+    company_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    company_telephone: Mapped[str] = mapped_column(String(15), nullable=False)
+    district_code: Mapped[int] = mapped_column(nullable=False)
+    up_first_time: Mapped[datetime.time] = mapped_column(Time, nullable=False)
+    up_last_time: Mapped[datetime.time] = mapped_column(Time, nullable=False)
+    down_first_time: Mapped[datetime.time] = mapped_column(Time, nullable=False)
+    down_last_time: Mapped[datetime.time] = mapped_column(Time, nullable=False)
+    start_stop_id: Mapped[int] = mapped_column(nullable=False)
+    end_stop_id: Mapped[int] = mapped_column(nullable=False)
+    route_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    route_type_code: Mapped[str] = mapped_column(String(10), nullable=False)
+    route_type_name: Mapped[str] = mapped_column(String(10), nullable=False)
+
+
+class BusStop(BaseModel):
+    __tablename__ = "bus_stop"
+    stop_id: Mapped[int] = mapped_column(primary_key=True)
+    stop_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    district_code: Mapped[int] = mapped_column(nullable=False)
+    mobile_number: Mapped[str] = mapped_column(String(15), nullable=False)
+    region_name: Mapped[str] = mapped_column(String(10), nullable=False)
+    latitude: Mapped[float] = mapped_column(nullable=False)
+    longitude: Mapped[float] = mapped_column(nullable=False)
 
 
 class BusRouteStop(BaseModel):
     __tablename__ = "bus_route_stop"
     __table_args__ = (PrimaryKeyConstraint("route_id", "stop_id"),)
-    route_id = Column(sqltypes.Integer, nullable=False)
-    stop_id = Column(sqltypes.Integer, nullable=False)
-    stop_sequence = Column(sqltypes.Integer, nullable=False)
+    route_id: Mapped[int] = mapped_column(ForeignKey("bus_route.route_id"), nullable=False)
+    stop_id: Mapped[int] = mapped_column(ForeignKey("bus_stop.stop_id"), nullable=False)
+    stop_sequence: Mapped[int] = mapped_column(nullable=False)
 
 
 class BusRealtime(BaseModel):
     __tablename__ = "bus_realtime"
     __table_args__ = (PrimaryKeyConstraint("route_id", "stop_id", "arrival_sequence"),)
-    stop_id = Column(sqltypes.Integer, nullable=False)
-    route_id = Column(sqltypes.Integer, nullable=False)
-    arrival_sequence = Column(sqltypes.Integer, nullable=False)
-    remaining_stop_count = Column(sqltypes.Integer, nullable=False)
-    remaining_seat_count = Column(sqltypes.Integer, nullable=False)
-    remaining_time = Column(sqltypes.Integer, nullable=False)
-    low_plate = Column(sqltypes.Boolean, nullable=False)
+    stop_id: Mapped[int] = mapped_column(ForeignKey("bus_route_stop.route_id"), nullable=False)
+    route_id: Mapped[int] = mapped_column(ForeignKey("bus_route_stop.stop_id"), nullable=False)
+    arrival_sequence: Mapped[int] = mapped_column(nullable=False)
+    remaining_stop_count: Mapped[int] = mapped_column(nullable=False)
+    remaining_seat_count: Mapped[int] = mapped_column(nullable=False)
+    remaining_time: Mapped[int] = mapped_column(nullable=False)
+    low_plate: Mapped[bool] = mapped_column(nullable=False)
