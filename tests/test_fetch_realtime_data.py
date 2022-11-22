4c11afd18e31ed4af7e056a317d41735121d56de
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-22 15:00:05 +0900

    feat: 버스 실시간 도착 정보 테스트 코드 추가

--- tests/test_fetch_realtime_data.py
+++ tests/test_fetch_realtime_data.py
@@ -1,8 +1,16 @@
+import asyncio
+from collections import defaultdict
+
 import pytest
+from sqlalchemy import select
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from models import BaseModel
+from models.bus import BusRealtime, BusRouteStop
+from scripts.realtime import get_realtime_data
+from tests.insert_bus_information import insert_bus_stop, insert_bus_route, insert_bus_route_stop, \
+    initialize_bus_data
 from utils.database import get_db_engine
 
 
@@ -20,9 +28,35 @@ class TestFetchRealtimeData:
         assert cls.session is not None
         # Migration schema check
         BaseModel.metadata.create_all(cls.connection)
+        # Insert initial data
+        asyncio.run(initialize_bus_data(cls.session))
         cls.session.commit()
         cls.session.close()
 
     @pytest.mark.asyncio
     async def test_fetch_realtime_data(self):
-        pass
+        connection = get_db_engine()
+        session_constructor = sessionmaker(bind=connection)
+        # Database session check
+        session = session_constructor()
+        # Get list to fetch
+        stop_group = defaultdict(list)
+        stop_query = select(BusRouteStop.stop_id, BusRouteStop.route_id)
+        session.execute(stop_query)
+        for stop_id, route_id in session.execute(stop_query):
+            stop_group[stop_id].append(route_id)
+        job_list = []
+        for stop_id, route_id_list in stop_group.items():
+            job_list.append(get_realtime_data(session, stop_id, route_id_list))
+        await asyncio.gather(*job_list)
+
+        # Check if the data is inserted
+        arrival_list = session.query(BusRealtime).all()
+        for arrival_item in arrival_list:  # type: BusRealtime
+            assert type(arrival_item.route_id) == int
+            assert type(arrival_item.stop_id) == int
+            assert type(arrival_item.arrival_sequence) == int
+            assert type(arrival_item.remaining_stop_count) == int
+            assert type(arrival_item.remaining_seat_count) == int
+            assert type(arrival_item.remaining_time) == int
+            assert type(arrival_item.low_plate) == bool
