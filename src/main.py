b42a0cb60bd7611aa2f0cc42f0edc3f236a3add2
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-01-31 16:37:56 +0900

    feat: Master 노드를 접속하기 위한 코드 구현

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import asyncio
 from collections import defaultdict
 
+import sqlalchemy
 from sqlalchemy import select, delete
 from sqlalchemy.orm import sessionmaker
 
 from models import BusRouteStop, BusRealtime
 from scripts.realtime import get_realtime_data
-from utils.database import get_db_engine
+from utils.database import get_db_engine, get_master_db_engine
 
 
 async def main():
@@ -15,7 +16,16 @@ async def main():
     session = session_constructor()
     if session is None:
         raise RuntimeError("Failed to get db session")
+    try:
+        await execute_script(session)
+    except sqlalchemy.exc.OperationalError:
+        connection = get_master_db_engine()
+        session_constructor = sessionmaker(bind=connection)
+        session = session_constructor()
+        await execute_script(session)
 
+
+async def execute_script(session):
     stop_group = defaultdict(list)
     stop_query = select(BusRouteStop.stop_id, BusRouteStop.route_id)
     session.execute(stop_query)
